3f26d912b00829d283de4403bd93706d9cf174c4
Volkan Şah <VolkanSah@users.noreply.github.com>
2024-05-25 17:31:59 +0200

    Create proxy_helper.py

---
+++ proxy_helper.py
@@ -0,0 +1,18 @@
+# proxy_helper.py
+def get_current_proxy():
+    try:
+        with open('current_proxy.txt', 'r') as file:
+            proxy = file.read().strip()
+        return proxy
+    except FileNotFoundError:
+        print("Proxy file not found.")
+        return None
+
+def get_proxies_dict():
+    proxy = get_current_proxy()
+    if proxy:
+        return {
+            'http': f'http://{proxy}',
+            'https': f'http://{proxy}',
+        }
+    return None
